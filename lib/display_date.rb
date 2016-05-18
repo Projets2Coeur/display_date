0ea21e05acf2f4a529e41af20142a04a943f6e7e
Jérémy FRERE <frere.jeremy@gmail.com>
2016-05-18 23:12:08 +0200

    Move /config to lib/config and require the files in a way that can be picked up when including the gem

--- lib/display_date.rb
+++ lib/display_date.rb
@@ -1,7 +1,7 @@
 require 'display_date/version'
 require 'i18n'
 
-I18n.load_path = Dir['config/locales/**/*.yml']
+I18n.load_path = Dir.glob( File.dirname(__FILE__) + "/config/locales/**/*.{rb,yml}" )
 I18n.backend.load_translations
 
 class DisplayDate
