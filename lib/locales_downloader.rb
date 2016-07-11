2a2a4b4637535dbf787db11d8526c98c7cbc274c
Jérémy FRERE <frere.jeremy@gmail.com>
2016-07-11 13:54:11 +0200

    Updates rails locales and includes day and month names

--- lib/locales_downloader.rb
+++ lib/locales_downloader.rb
@@ -6,20 +6,20 @@ Dotenv.load
 
 class LocalesDownloader
 
-  RAILS_LOCALES_DIR = 'config/locales/rails'
+  LOCALES_DIR = 'lib/config/locales/rails'
 
   class << self
 
     def call
       remove_all_locales
       fetch_latest_locales_from_rails_i18n_repo
-      remove_everything_not_related_to_date_and_time_formats_from_locale_files
+      remove_everything_not_related_to_date_and_time_from_locale_files
     end
 
   private
 
     def remove_all_locales
-      FileUtils.rm_rf("#{RAILS_LOCALES_DIR}/*")
+      FileUtils.rm_rf Dir.glob("#{LOCALES_DIR}/**/*")
     end
 
     def fetch_latest_locales_from_rails_i18n_repo
@@ -30,19 +30,18 @@ class LocalesDownloader
           repo_owner: 'svenfuchs',
           path: 'rails/locale'
         },
-        "#{RAILS_LOCALES_DIR}"
+        LOCALES_DIR
       )
     end
 
-    def remove_everything_not_related_to_date_and_time_formats_from_locale_files
-      Dir['config/locales/**/*'].each do |locale_file|
+    def remove_everything_not_related_to_date_and_time_from_locale_files
+      Dir["#{LOCALES_DIR}/**/*"].each do |locale_file|
         next if File.directory?(locale_file)
         original_hash = YAML.load(File.read(locale_file))
         language = original_hash.keys.first
         translation_hash = { language => {} }
-        translation_hash[language]["date"] = { "formats"     => original_hash[language]["date"]["formats"],
-                                               "month_names" => original_hash[language]["date"]["month_names"] }
-        translation_hash[language]["time"] = { "formats" => original_hash[language]["time"]["formats"] }
+        translation_hash[language]["date"] = original_hash[language]["date"]
+        translation_hash[language]["time"] = original_hash[language]["time"]
         File.open(locale_file, 'w') do |f|
           f << translation_hash.to_yaml
         end
